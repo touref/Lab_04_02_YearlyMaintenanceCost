87e0375e73d307398ab1117b923501078707e804
Fatou Toure <tourefo@mail.uc.edu>
2025-03-04 04:03:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int firstSeason = 25;
+        int secondSeason = 35;
+        int thirdSeason = 45;
+        int fourthSeason = 55;
+        int totalcost;
+        totalcost = (firstSeason + secondSeason + thirdSeason + fourthSeason);
+        System.out.println("The yearly maintenance costs are as follows:");
+        System.out.println("First Season: $" + firstSeason);
+        System.out.println("Second Season: $" + secondSeason);
+        System.out.println("Third Season: $" + thirdSeason);
+        System.out.println("Fourth Season: $" + fourthSeason);
+        System.out.println("The yearly maintenance costs is " + totalcost);
+    }
+}
